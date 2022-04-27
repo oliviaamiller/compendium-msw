327460b4d15ed32f6ab32d36dacf3eed6ec0e630
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-04-27 16:44:52 -0700

    fetched api in useEffect in List.jsx

--- src/views/Animals/List.jsx
+++ src/views/Animals/List.jsx
@@ -9,6 +9,25 @@ export default function AnimalsList() {
     const [filter, setFilter] = useState('');
     const [results, setResults] = useState([]);
     
+    useEffect(() => {
+        const getAnimals = async () => {
+            const res = await fetch('https://zoo-animal-api.herokuapp.com/animals/rand/10');
+            const { results } = await res.json();
+            const animalData = results.map((animal) => ({
+                img: animal.image_link,
+                name: animal.name,
+                latin: animal.latin_name,
+                lifespan: animal.lifespan,
+                habitat: animal.habitat
+            }));
+
+            setAnimals(animalData);
+            setLoading(false);
+        };
+        getAnimals();
+    }, []);
+
+    
   return (
     <div>AnimalsList</div>
   )
