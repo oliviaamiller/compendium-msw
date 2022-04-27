396b7aac74dcafacf51ca09a54f5b77a04b6ee3e
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-04-27 16:34:35 -0700

    set state in List.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import AnimalsList from './views/Animals/List';
+import './App.css';
 
 export default function App() {
   return (
--- src/views/Animals/List.jsx
+++ src/views/Animals/List.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import styles from '../../App.css';
+import AnimalCard from '../../components/Animals/Card';
+import { useEffect, useState } from 'react';
 
 export default function AnimalsList() {
+    const [animals, setAnimals] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [filter, setFilter] = useState('');
+    const [results, setResults] = useState([]);
+    
   return (
     <div>AnimalsList</div>
   )
