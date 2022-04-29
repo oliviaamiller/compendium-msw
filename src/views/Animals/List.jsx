871633ebfaf618786913df2b49d596bbaa48f323
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-04-29 11:32:04 -0700

    abc working

--- src/views/Animals/List.jsx
+++ src/views/Animals/List.jsx
@@ -8,11 +8,36 @@ import { fetchAnimals } from '../../services/Animals/ApiFetch';
 export default function AnimalsList() {
   const [animals, setAnimals] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [sort, setSort] = useState('all');
+  const [sort, setSort] = useState('');
   const [results, setResults] = useState([]);
 
+  const sortResults = results.length ? results : animals;
+
   const sortHandler = (e) => {
-    setSort(e.target.value);
+    e.preventDefault();
+    console.log(sort);
+    if (sort === 'abc') {
+      const abc = animals.sort((a, b) => {
+        let alower = a.name.toLowerCase();
+        let blower = b.name.toLowerCase();
+
+        if (alower < blower) {
+          return -1;
+        }
+        if (alower > blower) {
+          return 1;
+        }
+        return 0;
+      });
+      setResults(abc);
+      console.log(abc);
+    } else if (sort === 'lifespan') {
+      setResults(
+        animals.sort(function (a, b) {
+          return a.lifespan - b.lifespan;
+        })
+      );
+    }
   };
 
   useEffect(() => {
@@ -24,31 +49,19 @@ export default function AnimalsList() {
     getAnimals();
   }, []);
 
-  useEffect(() => {
-    if (value === 'abc') {
-      setResults(animals.name.sort());
-    } else if (value === 'lifespan') {
-      setResults(
-        animals.lifespan.sort(function (a, b) {
-          return a - b;
-        })
-      );
-    }
-  }, [sort]);
-
   return (
     <>
       <h2>Animals List</h2>
       <p className={styles.latin}>* refresh page for new animals!</p>
       <div>
-        <AnimalDropdown sortHandler={sortHandler} />
+        <AnimalDropdown sortHandler={sortHandler} setSort={setSort} />
       </div>
       <div>
         {loading ? (
           <p>loading...</p>
         ) : (
           <div className={styles.list}>
-            {animals.map((animal) => {
+            {sortResults.map((animal) => {
               return (
                 <AnimalCard
                   img={animal.img}
