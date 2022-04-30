941b4ea4454e6e3400a66db18e24909781c6443d
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-04-29 17:39:40 -0700

    user selecting dropdown option in msw test working!

--- src/views/Animals/List.test.jsx
+++ src/views/Animals/List.test.jsx
@@ -3,6 +3,8 @@ import ZooAnimals from './List';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { mockData } from '../../services/Animals/mockdata';
+import userEvent from '@testing-library/user-event';
+
 
 const server = setupServer(
   rest.get(
@@ -21,8 +23,8 @@ describe('animals list - component test', () => {
     await screen.findAllByAltText('zoo animal');
 
     waitFor(() => {
-        const nameTest = screen.getByText('Alligator Snapping Turtle');
-    })
+      const nameTest = screen.getByText('Alligator Snapping Turtle');
+    });
 
     const nameTest = await screen.findByText('Alligator Snapping Turtle');
 
@@ -31,11 +33,15 @@ describe('animals list - component test', () => {
 });
 
 describe('animals list - behavioral test', () => {
-    it('should sort animals by lifespan, short to long', async () => {
-        render(<ZooAnimals />)
+  it('should sort animals by lifespan, short to long', async () => {
+    render(<ZooAnimals />);
 
-        // find the dropdown button
-        screen.getByRole('button', { name: /sort/i });
+    // find the dropdown button
+    screen.getByRole('button', { name: /sort/i });
 
-    })
-})
+    // user selects to sort animal list by lifespan
+    userEvent.selectOptions(screen.getByRole('combobox'), 'lifespan');
+
+    expect(screen.getByText('lifespan short to long').selected).toBe(true);
+  });
+});
